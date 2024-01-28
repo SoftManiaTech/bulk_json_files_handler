68b574bbfe95ba8a6b83e16e64be0d6ca114e397
Muruganantham Pothanaickar <murugananthamp0104@gmail.com>
2024-01-28 12:01:44 +0000

    Working code for bulk json handling

--- bin/bulkJsonFilesHandler.py
+++ bin/bulkJsonFilesHandler.py
@@ -5,7 +5,9 @@ sys.path.append("./python-3.12.1/")
 from builtins import str
 import xml.dom.minidom, xml.sax.saxutils
 import os
+import glob
 import json
+import hashlib
 
 
 # Empty introspection routine
@@ -44,15 +46,39 @@ def get_path():
 
     return ""
 
+def file_mod(path,checkpoint_path):
+    os.chdir(path)
+    # print("Directory changed")
+    for file in glob.glob("*.json"):
+      # print(''' file name --> ''', file)
+      file_complete_path = path+"\\"+file
+      file_hash = str(hashlib.md5(file.encode()).hexdigest())+".txt"
+      path_hash = str(hashlib.md5(path.encode()).hexdigest())
+      checkpoint_path_final = os.path.join(checkpoint_path,path_hash,file_hash)
+      if not os.path.isfile(checkpoint_path_final):
+        #  print("Looks like a new file")
+         with open(file_complete_path, mode='r') as f:
+          data = json.load(f)
+          print(json.dumps(data))
+         os.chdir(checkpoint_path)
+         if not os.path.isdir(path_hash):
+          os.mkdir(path_hash)
+         os.chdir(os.path.join(checkpoint_path,path_hash))
+         file1 = open(file_hash, 'w+')
+         file1.close()
+      else:
+        pass
+        # print("This file was processed already")
+
+
 # Routine to index data
 def run_script():
-    # the folders with sample data structure
-    # python logic to index the list of json files
-    print("in run script")
-    # sys.exit(0)
-    for root, dirs, files in os.walk(os.getcwd()):
-        for file in files:
-            print(json.dumps({"file":file}))
+    base_dir = r'C:\Soft Mania\Usecase 3\mkdir'
+    checkpoint_path = r'C:\Program Files\SplunkUniversalForwarder\etc\apps\bulk_json_files_handler\checkpoints'
+    for root, dirs, files in os.walk(base_dir):
+        for dir in dirs:
+            path = os.path.join(base_dir,dir)
+            file_mod(path,checkpoint_path)
 
 # Script must implement these args: scheme, validate-arguments
 if __name__ == '__main__':
